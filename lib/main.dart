036ad2e2a825df49bef325cc9cdeb21c456ffd63
AbbasFaisalSsuet <abbas.faisal.ssuet@gmail.com>
2021-06-06 01:25:12 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:e_commerce_app/main_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    themeMode: ThemeMode.dark,
+    darkTheme: ThemeData(
+      brightness: Brightness.dark,
+    ),
+    home: MyApp(),
+  ));
+}
